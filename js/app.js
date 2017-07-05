3da3aaa6708a33970b4a0e0d857f71006006017a
Jakub Placek <jakub.placek@o2.pl>
2017-07-05 14:54:37 +0200

    Some changes

--- js/app.js
+++ js/app.js
@@ -107,13 +107,10 @@ $.fn.jQuerySimpleCounter = function( options ) {
 	};
 
 
-$('#number1').jQuerySimpleCounter({end: 12,duration: 7000});
-$('#number2').jQuerySimpleCounter({end: 55,duration: 7000});
-$('#number3').jQuerySimpleCounter({end: 359,duration: 7000});
-$('#number4').jQuerySimpleCounter({end: 246,duration: 7000});
-
-
-
+setTimeout(function(){$('#number1').jQuerySimpleCounter({end: 12,duration: 7000})}, 6500);
+setTimeout(function(){$('#number2').jQuerySimpleCounter({end: 55,duration: 7000})}, 6500);
+setTimeout(function(){$('#number3').jQuerySimpleCounter({end: 359,duration: 7000})}, 6500);
+setTimeout(function(){$('#number4').jQuerySimpleCounter({end: 246,duration: 7000})}, 6500);
 
 
 });
